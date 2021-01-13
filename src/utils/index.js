18564eb04a3efc86d878fec8cdc74025b6364fa9
yydd199706 <2287659359@qq.com>
2021-01-13 15:34:18 +0800

    修改app.vue index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,62 @@
-function formatNumber (n) {
-  const str = n.toString()
-  return str[1] ? str : `0${str}`
-}
+const app = getApp()
 
-export function formatTime (date) {
+const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
-
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  const t1 = [year, month, day].map(formatNumber).join('/')
-  const t2 = [hour, minute, second].map(formatNumber).join(':')
-
-  return `${t1} ${t2}`
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-export default {
-  formatNumber,
-  formatTime
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+
+
+function checkSession() {
+  wx.checkSession({
+    success: function() {
+      //session_key 未过期，并且在本生命周期一直有效
+      console.log('session_key 未过期！')
+    },
+    fail: function() {
+      // session_key 已经失效，需要重新执行登录流程
+      wx.login({
+        success: function(res) {
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: app.globalData.url+'WxLogin/GetSessionKey',
+              data: {
+                code: res.code
+              },
+              method: 'POST',
+              success: function(res) {
+                //app.globalData.sessionKey = res.data;
+                wx.setStorage({
+                  key: 'sessionKey',
+                  data: res.data,
+                })
+              },fail: function (res) {
+                console.log("失败："+JSON.stringify(res));
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+    }
+  })
+}
+
+
+export{
+  formatTime,
+  checkSession
 }
