7d1ddfefcf5f9defd3402f96a7d5eef552dcd15e
yydd199706 <2287659359@qq.com>
2021-02-25 16:31:12 +0800

    更改文件名称

---
+++ src/pages/guanzhu/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <div>
+    <div>立即关注</div>
+    <div>房源降价早知道</div>
+  </div>
+  <div>
+    
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      };
+    }
+  }
+</script>
+<style scoped>
+ 
+</style>
