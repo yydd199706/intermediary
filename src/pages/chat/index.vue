95daa7ca3a55f9ee4922385176260dbd037ea079
yydd199706 <2287659359@qq.com>
2021-04-26 15:46:58 +0800

    2021-4-26

--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -3,25 +3,31 @@
 
     <div class="chat">
       <scroll-view scroll-y="true" class='padding20' style="height:1000rpx;width:auto;"> 
-        <!-- 时间 -->
-        <div class="timetop">
-          <div class="time_chat">14:48</div>
-        </div>
-
-        <!-- 内容 -->
-        <div class="content_chat">
-          <!-- 我发出的内容 -->
-          <div class="meIssue">
-            <image :src="item.img1" class="me_img" />
-            <div class="chat_list">
-              <div class="me_text">{{item.text}}</div>
-            </div>
+        <block v-for="(item, index) in chatList" :key="index">
+          <!-- 时间 -->
+          <div class="timetop">
+            <div class="time_chat">14:48</div>
           </div>
-          <!-- 对方发出的内容 -->
-          <div class="otherParty"></div>
-        </div>
 
+          <!-- 内容开始 -->
+          <div class="content_chat">
+            <!-- 我发出的内容 -->
+            <div class="meIssue">
+              <image :src="item.img1" class="me_img" />
+              <div class="chat_list">
+                <div class="me_text">{{item.text}}</div>
+                <image :src="item.img2" class="me_img" />
+              </div>
+            </div>
+            <!-- 对方发出的内容 -->
+            <div class="otherParty">
+              <image :src="item.img1" class="me_img" />
+            </div>
 
+          </div>
+          <!-- 内容结束-->
+
+        </block>
       </scroll-view>
     </div>
 
@@ -42,6 +48,9 @@
 export default {
   data () {
     return {
+      chatList:[
+        {img1:"/static/images/meimg.png",text:"您好",img2:"/static/images/sj.png"}
+      ]
      
 
 
@@ -63,9 +72,16 @@ export default {
   height: 0;
   display: block;}
 .hsxian{ width: 100%; height:20rpx; background: #f8f8fa;}
-.indexstyle{width: 100%; margin: 0 auto; background: #fff;}
- 
+.indexstyle{width: 100%; margin: 0 auto; background: #f1f1f1;}
 
+.chat{ width: 100%; margin: 0 auto;}
+.timetop{ width: 100rpx; height: 60rpx; line-height: 60rpx; background: #dadada; border-radius:6rpx; margin: 0 auto; margin-top: 30rpx;}
+.time_chat{ text-align: center; color: #fff; font-size: 28rpx;}
+.content_chat{ width: 90%; margin-left: 5%; margin-right: 5%;}
+/* .meIssue{ width: 70%;} */
+.meIssue image{ width: 65rpx; height: 65rpx;} 
+.meIssue .me_text{max-width: 70%; background: #fff; padding: 20rpx; border-radius: 10rpx; margin-right:20rpx;}
+.chat_list image{ width:14rpx; height:22rpx;}
 
 
 </style>
